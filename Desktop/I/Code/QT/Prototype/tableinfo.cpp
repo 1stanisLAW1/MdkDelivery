ec7d1f89cbb36fea34e2f43625a6a4854f292473
stanisLAW <stasmazur715@gmail.com>
2025-03-04 20:55:58 +0500

    Delete Desktop/I/Code/QT/Prototype directory

--- Desktop/I/Code/QT/Prototype/tableinfo.cpp
+++
@@ -1,9 +0,0 @@
-#include "tableinfo.h"
-#include <QTableWidget>
-
-TableInfo::TableInfo(Moduls *)
-{
-    QTableWidget *table = new QTableWidget;
-    table->setRowCount(5);
-    table->setColumnCount(2);
-}
